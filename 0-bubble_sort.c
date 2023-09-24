ab00fa1bb8e17fa72a383613ef1b5f20975d98c5
elvismalsa <elvisotienomboya@gmail.com>
2023-09-24 13:22:14 +0300

    corrections made

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,29 +1,30 @@
 #include "sort.h"
 
+/**
+ * bubble_sort - function that sorts an array of integers in ascending order
+ * @array: the arrey to be sorted
+ * @size: the size
+ *
+ *
+ */
 void bubble_sort(int *array, size_t size)
 {
-        long unsigned int i;
-	long unsigned int j;
-	long unsigned int temp;
-	long unsigned int k;
-	for (i = 0; i < size; i++)
-        {
-                for (j = 0; j < (size -1); j++)
-                {
-                        if (array[j] > array[j + 1])
-                        {
-                                temp = array[j];
-                                array[j] = array[j + 1];
-                                array[j + 1] = temp;
+	size_t x, y;
+	int num;
 
-                        }
-                }
-
-		printf("%lu\n", i + 1);
-		for (k = 0; k < size; k++)
+	if (!array || size < 2)
+		return;
+	for (y = 0; y < size - 1; y++)
+	{
+		for (x = 0; x < size - y - 1; x++)
 		{
-			printf("%u, ", array[k]);
+			if (array[x] > array[x + 1])
+			{
+				num = array[x];
+				array[x] = array[x + 1];
+				array[x + 1] = num;
+				print_array(array, size);
+			}
 		}
 	}
-
 }
--- sort.h
+++ sort.h
@@ -1,6 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stddef.h>
+
 #include <stdio.h>
 
 /**
@@ -20,4 +20,5 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+
 #endif
