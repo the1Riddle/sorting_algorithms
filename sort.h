171a6de1a5747825a09c671f679d6622656a0c7a
the1Riddle <elvisotienomboya@gmail.com>
2023-09-25 09:17:57 -0700

    the main header file

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -21,8 +22,14 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
+
+/****** that returns ints *******/
+int lomuto_pertition(int *array, int low, int hi, size_t size);
+void sort_lomuto(int *array, int low, int hi, size_t size);
 
 /*** used to swap the nodes ***/
 listint_t *swap_node(listint_t *node, listint_t **list);
+void swap_elements(int *a, int *b);
 
 #endif
