0f1e3407defbd1867d79325e3ce7054578a03cba
Heavenboi <nigelzenda98@gmail.com>
2023-09-26 15:15:24 +0000

    fixed section_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,23 @@
-#include <stdio.h>
+#include "sort.h"
 
 void selection_sort(int *array, size_t size)
 {
-	for (i = 0; i < 7; i++) 
+	int j, i, min, temp;
+	for (i = 0; i < 9; i++) 
 	{
-        min = i;
-        for (j = i + 1; j < 8; j++)
-	{
-            if (array[j] < array[min]) 
-	    {
-                min = j;
-            }
-        }
-        if (min != i) {
-            temp = array[min];
-            array[min] = array[i];
-            array[i] = temp;
+		min = i;
+		for (j = i + 1; j < 10; j++)
+		{
+			if (array[j] < array[min]) 
+				min = j;
+		}
+		if (min != i) 
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
+	}
 
-            printf("Swapped array[%d] %d and array[%d] %d\n", i, array[i], min, array[min]);
-        }
-    }
-
-    printf("Sorted array: ");
-    for (i = 0; i < 8; i++) {
-        printf("%d ", array[i]);
-    }
-
-    return (0);
 }
