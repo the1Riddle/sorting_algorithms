1a6bd9aa279b177562b05200f6b54716dacaf4ac
the1Riddle <elvisotienomboya@gmail.com>
2023-09-27 12:13:44 -0700

    for testing a file

---
+++ test.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	shell_sort(NULL, 0);
+	return (0);
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {1};
+    size_t n = sizeof(array) / sizeof(array[0]);/* 1 */
+
+    print_array(array, n);
+    printf("\n");
+    shell_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
